dd9f82012c1e4bdf7c179cc8ca8418eb920ab60b
Stanislav Dashkov <stdashkov@mail.ru>
2020-03-02 20:37:02 +0300

    add test home redux

--- src/Pages/Homepage/HomePage.js
+++ src/Pages/Homepage/HomePage.js
@@ -1,22 +1,46 @@
 import React from "react";
 import { connect } from "react-redux";
 
+import {
+  showHomeItem,
+  addHomeItem,
+  minusHomeItem
+} from "redux/home/home.actions";
 import Directory from "components/directory";
 import "./HomePage.scss";
 
-const HomePage = () => (
-  <div className="homepage">
-    <h1>Welcome to my Homepage</h1>
+const HomePage = ({ homeItem, showHomeItem, addHomeItem, minusHomeItem }) => {
+  return (
+    <div className="homepage">
+      <h1>Welcome to my Homepage </h1>
+      {homeItem.showItem ? (
+        <div>
+          <button onClick={minusHomeItem}>-</button>
 
-    <Directory />
-  </div>
-);
+          <span onClick={showHomeItem}>{homeItem.value}</span>
+
+          <button onClick={addHomeItem}>+</button>
+        </div>
+      ) : (
+        <span onClick={showHomeItem}>None</span>
+      )}
+      <Directory />
+    </div>
+  );
+};
 
 const mapStateToProps = state => {
   return {
     user: state.user,
-    cart: state.cart.cart
+    cart: state.cart.cart,
+    homeItem: state.homeItem
   };
 };
 
-export default connect(mapStateToProps)(HomePage);
+const mapDispatchToProps = dispatch => ({
+  showHomeItem: () => dispatch(showHomeItem()),
+  addHomeItem: () => dispatch(addHomeItem()),
+  minusHomeItem: () => dispatch(minusHomeItem())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,7 @@ import logger from "redux-logger";
 import RootReducer from "./RootReducer";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const middlewares = [logger];
+const middlewares = [];
 
 export const store = createStore(
   RootReducer,
